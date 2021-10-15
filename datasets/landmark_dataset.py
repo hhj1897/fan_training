5131eebc3b4511a5c88a8888f827c632b33e0246
hhj1897 <js1907@imperial.ac.uk>
2021-10-15 18:09:21 +0100

    Change the dataset class to include the old sample preparation method, also update evaluation code.

--- datasets/landmark_dataset.py
+++ datasets/landmark_dataset.py
@@ -11,8 +11,8 @@ from utils import load_pts, flip_landmarks, encode_landmarks
 
 
 class LandmarkDataset(Dataset):
-    def __init__(self, tsv_path, partitions, config=None, geometric_transform=None,
-                 content_transform=None, random_flip=False):
+    def __init__(self, tsv_path, partitions, config=None, random_flip=False,
+                 geometric_transform=None, content_transform=None):
         self._samples = []
         self.weight_norm = 0.0
         tsv_folder = os.path.dirname(tsv_path)
@@ -37,18 +37,20 @@ class LandmarkDataset(Dataset):
             self.config = LandmarkDataset.create_config()
         else:
             self.config = config
+        self.random_flip = random_flip
         self.geometric_transform = geometric_transform
         self.content_transform = content_transform
-        self.random_flip = random_flip
 
     @staticmethod
     def create_config(image_size=256, heatmap_size=64, heatmap_gaussian_size=5, heatmap_gaussian_sigma=1,
-                      heatmap_with_subpixel_sampling=True, crop_ratio=0.55, temp_padding_factor=1):
+                      heatmap_with_subpixel_sampling=True, crop_ratio=0.55, temp_padding_factor=1,
+                      use_improved_preprocessing=True):
         return SimpleNamespace(image_size=image_size, heatmap_size=heatmap_size,
                                heatmap_gaussian_size=heatmap_gaussian_size,
                                heatmap_gaussian_sigma=heatmap_gaussian_sigma,
                                heatmap_with_subpixel_sampling=heatmap_with_subpixel_sampling,
-                               crop_ratio=crop_ratio, temp_padding_factor=temp_padding_factor)
+                               crop_ratio=crop_ratio, temp_padding_factor=temp_padding_factor,
+                               use_improved_preprocessing=use_improved_preprocessing)
 
     @staticmethod
     def get_partitions(dataset, split):
@@ -81,52 +83,99 @@ class LandmarkDataset(Dataset):
         image = cv2.cvtColor(cv2.imread(self._samples[item]['im_path']), cv2.COLOR_BGR2RGB)
         landmarks = load_pts(self._samples[item]['pts_path'])
 
-        # Create the transform to properly crop out the face region
+        # Random flip needs to be handled manually
         im_hight, im_width = image.shape[:2]
         face_box = self._samples[item]['face_box']
+        if self.random_flip and random.random() < 0.5:
+            landmarks = flip_landmarks(landmarks, im_width)
+            face_box = np.array([[im_width - face_box[1, 0], face_box[0, 1]],
+                                 [im_width - face_box[0, 0], face_box[1, 1]]])
+            image = cv2.flip(image, 1)
+        face_corners = np.array([face_box[0], [face_box[1, 0], face_box[0, 1]],
+                                 face_box[1], [face_box[0, 0], face_box[1, 1]]])
+
         face_centre = face_box.mean(axis=0)
         face_size = (face_box[1] - face_box[0]).mean()
         crop_box_size = face_size / self.config.crop_ratio
-        crop_box_tl = np.round(face_centre - crop_box_size / 2.0 * (1 + self.config.temp_padding_factor))
-        crop_box_br = crop_box_tl + np.round(crop_box_size * (1 + self.config.temp_padding_factor)) + 1
-        crop_margin = (int(-crop_box_tl[1]), int(crop_box_br[0] - im_width),
-                       int(crop_box_br[1] - im_hight), int(-crop_box_tl[0]))
-        crop_trans = augs.Compose(
-            [augs.CropAndPad(crop_margin),
-             augs.Resize(int(np.round(self.config.image_size * (1 + self.config.temp_padding_factor))),
-                         int(np.round(self.config.image_size * (1 + self.config.temp_padding_factor))))])
+        keypoint_params = augs.KeypointParams(format='xy', remove_invisible=False)
+        if self.config.use_improved_preprocessing:
+            # Create the transform to crop out the face region with some margin added
+            crop_box_tl = np.round(face_centre - crop_box_size / 2.0 * (1 + self.config.temp_padding_factor))
+            crop_box_br = crop_box_tl + np.round(crop_box_size * (1 + self.config.temp_padding_factor)) + 1
+            crop_margin = (int(-crop_box_tl[1]), int(crop_box_br[0] - im_width),
+                           int(crop_box_br[1] - im_hight), int(-crop_box_tl[0]))
+            crop_trans = augs.Compose(
+                [augs.CropAndPad(crop_margin, pad_mode=cv2.BORDER_CONSTANT),
+                 augs.Resize(int(np.round(self.config.image_size * (1 + self.config.temp_padding_factor))),
+                             int(np.round(self.config.image_size * (1 + self.config.temp_padding_factor))))])
 
-        # Add other data augmentation transforms
-        all_transforms = [crop_trans]
-        if self.geometric_transform is not None:
-            all_transforms.append(self.geometric_transform)
-        all_transforms.append(augs.CenterCrop(self.config.image_size, self.config.image_size))
-        if self.content_transform is not None:
-            all_transforms.append(self.content_transform)
-        composite_trans = augs.Compose(
-            all_transforms, keypoint_params=augs.KeypointParams(format='xy', remove_invisible=False))
+            # Add other data augmentation transforms
+            all_transforms = [crop_trans]
+            if self.geometric_transform is not None:
+                all_transforms.append(self.geometric_transform)
+            all_transforms.append(augs.CenterCrop(self.config.image_size, self.config.image_size))
+            if self.content_transform is not None:
+                all_transforms.append(self.content_transform)
+            composite_trans = augs.Compose(all_transforms, keypoint_params=keypoint_params)
 
-        # Transform the image and the landmarks
-        face_corners = [face_box[0], [face_box[1, 0], face_box[0, 1]], face_box[1], [face_box[0, 0], face_box[1, 1]]]
-        trans_res = composite_trans(image=image, keypoints=np.vstack((landmarks, face_corners)))
-        image, warped_keypoints = trans_res['image'], trans_res['keypoints']
-        landmarks = np.array(warped_keypoints[:-4])
-        face_corners = np.array(warped_keypoints[-4:])
+            # Transform the image and the landmarks
+            trans_res = composite_trans(image=image, keypoints=np.vstack((landmarks, face_corners)))
+            image, warped_keypoints = trans_res['image'], trans_res['keypoints']
+            landmarks = np.array(warped_keypoints[:landmarks.shape[0]])
+            face_corners = np.array(warped_keypoints[-face_corners.shape[0]:])
 
-        # Random flip needs to be handled manually
-        if self.random_flip and random.random() < 0.5:
-            landmarks = flip_landmarks(landmarks, image.shape[1])
-            face_corners[:, 0] = image.shape[1] - face_corners[:, 0]
-            face_corners = np.ascontiguousarray(face_corners[[1, 0, 3, 2]])
-            image = cv2.flip(image, 1)
+            # Create the label heatmaps
+            heatmaps = encode_landmarks(landmarks / self.config.image_size * self.config.heatmap_size,
+                                        self.config.heatmap_size, self.config.heatmap_size,
+                                        self.config.heatmap_gaussian_size, self.config.heatmap_gaussian_sigma,
+                                        self.config.heatmap_with_subpixel_sampling)
+        else:
+            # Crop the image
+            crop_box_tl = np.round(face_centre - crop_box_size / 2.0)
+            crop_box_br = crop_box_tl + np.round(crop_box_size) + 1
+            crop_margin = (int(-crop_box_tl[1]), int(crop_box_br[0] - im_width),
+                           int(crop_box_br[1] - im_hight), int(-crop_box_tl[0]))
+            crop_trans = augs.Compose([augs.CropAndPad(crop_margin, pad_mode=cv2.BORDER_CONSTANT),
+                                       augs.Resize(self.config.image_size, self.config.image_size)],
+                                      keypoint_params=keypoint_params)
+            crop_res = crop_trans(image=image, keypoints=np.vstack((landmarks, face_corners)))
+            image, warped_keypoints = crop_res['image'], crop_res['keypoints']
+            landmarks = np.array(warped_keypoints[:landmarks.shape[0]])
+            face_corners = np.array(warped_keypoints[-face_corners.shape[0]:])
+
+            # Create the label heatmaps
+            heatmaps = encode_landmarks(landmarks / self.config.image_size * self.config.heatmap_size,
+                                        self.config.heatmap_size, self.config.heatmap_size,
+                                        self.config.heatmap_gaussian_size, self.config.heatmap_gaussian_sigma,
+                                        self.config.heatmap_with_subpixel_sampling)
+
+            if self.geometric_transform is not None:
+                # Compose the geometric transform, also with some margin added
+                padded_image_size = int(round(self.config.image_size * (1 + self.config.temp_padding_factor)))
+                composite_trans = augs.ReplayCompose(
+                    [augs.PadIfNeeded(padded_image_size, padded_image_size, border_mode=cv2.BORDER_CONSTANT),
+                     self.geometric_transform, augs.CenterCrop(self.config.image_size, self.config.image_size)],
+                    keypoint_params=keypoint_params)
+
+                # Apply the composite geometric transform to the image and the landmarks
+                trans_res = composite_trans(image=image, keypoints=np.vstack((landmarks, face_corners)))
+                image, warped_keypoints = trans_res['image'], trans_res['keypoints']
+                landmarks = np.array(warped_keypoints[:landmarks.shape[0]])
+                face_corners = np.array(warped_keypoints[-face_corners.shape[0]:])
+
+                # Apply the same transform to the heatmaps
+                heatmaps = augs.resize(heatmaps.numpy().transpose(1, 2, 0),
+                                       self.config.image_size, self.config.image_size)
+                heatmaps = augs.resize(augs.ReplayCompose.replay(trans_res['replay'], image=heatmaps,
+                                                                 keypoints=[])['image'],
+                                       self.config.heatmap_size, self.config.heatmap_size)
+                heatmaps = torch.from_numpy(heatmaps.transpose(2, 0, 1))
+
+            # Apply the content transform to the image
+            if self.content_transform is not None:
+                image = self.content_transform(image=image)['image']
 
         # Image to tensor
-        image = torch.from_numpy(image.astype(np.float32).transpose((2, 0, 1))).div_(255.0)
-
-        # Create the label heatmaps
-        heatmaps = encode_landmarks(landmarks / self.config.image_size * self.config.heatmap_size,
-                                    self.config.heatmap_size, self.config.heatmap_size,
-                                    self.config.heatmap_gaussian_size, self.config.heatmap_gaussian_sigma,
-                                    self.config.heatmap_with_subpixel_sampling)
+        image = torch.from_numpy(image.astype(np.float32).transpose((2, 0, 1))).div_(255)
 
         return image, heatmaps, torch.from_numpy(landmarks), torch.from_numpy(face_corners), self._samples[item]
--- evaluation.py
+++ evaluation.py
@@ -1,44 +1,76 @@
-import torch
 import sklearn
 import numpy as np
 from utils import decode_landmarks
 
 
-__all__ = ['evaluate_model']
+__all__ = ['run_model_to_compute_errors', 'compute_auc']
 
 
-def evaluate_model(model, data_loader, threshold, norm_by_face_height=False, use_sample_weight=False, pbar=None):
-    all_errors = []
-    device = next(model.parameters()).device
+def run_model_to_compute_errors(model, data_loader, pbar=None):
+    all_errors = {'heatmap_errors': np.array([]), 'losses': np.array([]), 'point_errors': np.array([]),
+                  'iods': np.array([]), 'face_heights': np.array([]), 'sample_weights': np.array([])}
     if pbar is not None:
         pbar.reset(total=len(data_loader))
-    for images, _, landmarks, face_corners, metadata in data_loader:
+    device = next(model.parameters()).device
+    was_training = model.training
+    model.eval()
+    for images, heatmaps, landmarks, face_corners, metadata in data_loader:
         images = images.to(device)
+        heatmaps = heatmaps.to(device)
         landmarks = landmarks.to(device)
-        face_corners = face_corners.to(device)
-        heatmaps = model(images)[-1]
-        predicted_landmarks = decode_landmarks(heatmaps)[0]
-        predicted_landmarks[..., 0] *= images.shape[-1] / heatmaps.shape[-1]
-        predicted_landmarks[..., 1] *= images.shape[-2] / heatmaps.shape[-2]
-        errors = ((predicted_landmarks - landmarks).norm(dim=-1)).mean(dim=-1)
-        if norm_by_face_height:
-            # Normalise by Face height
-            norm_factors = (face_corners[:, 2] - face_corners[:, 1]).norm(dim=-1)
-        else:
-            # Normalise by IOD
-            norm_factors = (landmarks[:, 45] - landmarks[:, 36]).norm(dim=-1)
-        norm_factors.clamp_min(torch.finfo(norm_factors.dtype).eps)
-        normalised_errors = errors / norm_factors
-        errors = normalised_errors.detach().cpu().numpy().tolist()
-        if use_sample_weight:
-            for error, weight in zip(errors, metadata['weight']):
-                all_errors += [error] * max(1, int(weight))
-        else:
-            all_errors += errors
+
+        # Predict heatmaps
+        preds = model(images)
+
+        # Compute heatmap errors
+        loss = ((preds[-1] - heatmaps) ** 2).mean(dim=(1, 2, 3))
+        all_errors['heatmap_errors'] = np.concatenate(
+            (all_errors['heatmap_errors'], loss.detach().cpu().numpy()))
+
+        # Compute MSE losses (with intermediate supervision)
+        for pred in preds[:-1]:
+            loss += ((pred - heatmaps) ** 2).mean(dim=(1, 2, 3))
+        loss /= len(preds)
+        all_errors['losses'] = np.concatenate((all_errors['losses'], loss.detach().cpu().numpy()))
+
+        # Compute points errors
+        predicted_landmarks = decode_landmarks(preds[-1])[0]
+        predicted_landmarks[..., 0] *= images.shape[-1] / preds[-1].shape[-1]
+        predicted_landmarks[..., 1] *= images.shape[-2] / preds[-1].shape[-2]
+        all_errors['point_errors'] = np.concatenate(
+            (all_errors['point_errors'],
+             ((predicted_landmarks - landmarks).norm(dim=-1)).mean(dim=-1).detach().cpu().numpy()))
+
+        # Compute IODs
+        all_errors['iods'] = np.concatenate(
+            (all_errors['iods'],
+             (landmarks[:, 45] - landmarks[:, 36]).norm(dim=-1).detach().cpu().numpy()))
+
+        # Compute face heights
+        all_errors['face_heights'] = np.concatenate(
+            (all_errors['face_heights'],
+             (face_corners[:, 2] - face_corners[:, 1]).norm(dim=-1).detach().cpu().numpy()))
+
+        # Record sample weights
+        all_errors['sample_weights'] = np.concatenate(
+            (all_errors['sample_weights'], metadata['weight'].detach().cpu().numpy()))
+
         if pbar is not None:
             pbar.update(1)
+
+    if was_training:
+        model.train()
     if pbar is not None:
         pbar.close()
+    return all_errors
+
+
+def compute_auc(all_errors, threshold, sample_weights=None):
+    if sample_weights is not None:
+        resampled_errors = []
+        for error, weight in zip(all_errors, sample_weights):
+            resampled_errors += [error] * max(1, int(weight))
+        all_errors = resampled_errors
 
     # Compute AUC
     auc_metric = 0.0
